de04ce3603b55f55d6d8dc408d4b9d7835f400db
SkyNokK <jean.ferro@ynov.com>
2022-01-28 16:57:01 +0100

    Favoris fini + géolocalisation modifié avec celle proposé par expo, manque : autocomplétion

--- projetMeteo/Pages/Favoris/index.tsx
+++ projetMeteo/Pages/Favoris/index.tsx
@@ -1,103 +1,113 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button, Image, FlatList, TouchableOpacity } from 'react-native';
-
-//Ajouter la température maximum et minimum de la journée
-const Item = ({ nomVille }: any) => (
-    <View style={styles.item}>
-      <Text style={styles.textInfo}>{nomVille}</Text>
-    </View>
-  );
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Favoris = ({ navigation }: any) => {
     var [favoris, setFavoris] = useState([]);
+    var tableauVille: { Nom: any }[] = [];
 
     const apiKey = "032cf4f8f4ea0479f5a02354c7508195";
 
     //Fonction afin de récupérer les favoris
     useEffect(() => {
-      //
+        async function AsyncFonction() {
+            await recupData();
+            //console.log(tableauVille);
+        }
+        AsyncFonction();
     }, []);
 
-    var tableauVille: { Nom: any}[] = [];
-    tableauVille.push({Nom: "Aix-En-Provence"});
-    tableauVille.push({Nom: "Marseille"});
+    const recupData = async () => {
+        //tableauVille = [];
+
+        let jsonValue = await AsyncStorage.getItem('@favoris');
+        jsonValue = jsonValue != null ? JSON.parse(jsonValue) : null;
+        // console.log(jsonValue);
+
+        if (jsonValue != null) {
+            //var tabFavoris = jsonValue.replace("\"", "");
+            //var tabSplit = tabFavoris.split(",");
+            for (let k of jsonValue) {
+                if (k != "") {
+                    tableauVille.push({ Nom: k });
+                }
+            }
+
+        } else {
+            //Pas de favoris
+        }
+
+    }
+
+    tableauVille.push({ Nom: "" });
 
     var MeteoTableau: { Timestamp: any, Date: any; Temps: any; Temperature: any; Image: any }[] = [];
 
     //Ouverture de la ville
-    const favorisOuverture = (ville : any) => {
+    const favorisOuverture = (ville: any) => {
         MeteoTableau = [];
 
-        fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + ville + '&units=metric&appid=' + apiKey)
-        .then((response) => response.json())
-        .then((json) => {
-            var date = null;
-            var temps = null;
-            var temperature = null;
-        
-            for(let k of json.list){
-                //console.log(date);
-                if(date == null){
-                date = k.dt_txt;
-                var dateSplit = date.split(' ');
-                date = dateSplit[0];
-                temps = k.weather[0].main;
-                temperature = k.main.temp;
-                
-                var dateSplitFormat = date.split('-');
-                var dateFinal = dateSplitFormat[2]+"/"+dateSplitFormat[1]+"/"+dateSplitFormat[0];
-                
-                var timeStamp = k.dt;
+        if (ville != "") {
+            fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + ville + '&units=metric&appid=' + apiKey)
+                .then((response) => response.json())
+                .then((json) => {
+                    var date = null;
+                    var temps = null;
+                    var temperature = null;
 
-                MeteoTableau.push({Timestamp: timeStamp ,Date: dateFinal, Temps: temps, Temperature: temperature, Image: null});
-                }else{
-                    var date2 = k.dt_txt;
-                    var dateSplit2 = date2.split(' ');
-                    date2 = dateSplit2[0];
-                    if(date == date2){
-                        //Même jour
-                    }else{
-                        date = date2;
-                        temps = k.weather[0].main;
-                        temperature = k.main.temp;
-                        
-                        var date2SplitFormat = date2.split('-');
-                        var dateFinal = date2SplitFormat[2]+"/"+date2SplitFormat[1]+"/"+date2SplitFormat[0];
-            
-                        var timeStamp = k.dt;
+                    for (let k of json.list) {
+                        //console.log(date);
+                        if (date == null) {
+                            date = k.dt_txt;
+                            var dateSplit = date.split(' ');
+                            date = dateSplit[0];
+                            temps = k.weather[0].main;
+                            temperature = k.main.temp;
 
-                        MeteoTableau.push({Timestamp: timeStamp, Date: dateFinal, Temps: temps, Temperature: temperature, Image: null});
+                            var dateSplitFormat = date.split('-');
+                            var dateFinal = dateSplitFormat[2] + "/" + dateSplitFormat[1] + "/" + dateSplitFormat[0];
+
+                            var timeStamp = k.dt;
+
+                            MeteoTableau.push({ Timestamp: timeStamp, Date: dateFinal, Temps: temps, Temperature: temperature, Image: null });
+                        } else {
+                            var date2 = k.dt_txt;
+                            var dateSplit2 = date2.split(' ');
+                            date2 = dateSplit2[0];
+                            if (date == date2) {
+                                //Même jour
+                            } else {
+                                date = date2;
+                                temps = k.weather[0].main;
+                                temperature = k.main.temp;
+
+                                var date2SplitFormat = date2.split('-');
+                                var dateFinal = date2SplitFormat[2] + "/" + date2SplitFormat[1] + "/" + date2SplitFormat[0];
+
+                                var timeStamp = k.dt;
+
+                                MeteoTableau.push({ Timestamp: timeStamp, Date: dateFinal, Temps: temps, Temperature: temperature, Image: null });
+                            }
+                        }
+                        navigation.navigate("info", { ville: ville, tableau: MeteoTableau });
                     }
-                }
-                navigation.navigate("info", { ville: ville, tableau: MeteoTableau });
-            }
-        })
+                })
+        }
     }
 
-    const renderItem = ({ item }: any) => (
-        <Item nomVille={item.Nom}/>
-    );
-    
     return (
         <View style={styles.container}>
-
-                {/* <FlatList
-                    data={tableauVille}
-                    renderItem={renderItem}
-                    keyExtractor={(item) => item.Nom}
-                    
-                /> */}
-
-                <FlatList
-                    data={tableauVille}
-                    renderItem={({item}) => (
-                        <TouchableOpacity onPress={ () => {favorisOuverture(item.Nom)}}>
-                            <View style={styles.item}>
-                                <Text style={styles.textInfo}>{item.Nom}</Text>
-                            </View>
-                        </TouchableOpacity>
-                    )}
-                    /> 
+            <FlatList
+                data={tableauVille}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => { favorisOuverture(item.Nom) }}>
+                        <View style={styles.item}>
+                            {item.Nom != '' ? <Text style={styles.textInfo}>{item.Nom}</Text> : null}
+                        </View>
+                    </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.Nom}
+            />
         </View>
     );
 }
@@ -114,13 +124,13 @@ const styles = StyleSheet.create({
         margin: 5,
         alignSelf: 'flex-end',
     },
-    textTitre:{
+    textTitre: {
         fontSize: 20,
         color: 'white',
         textAlign: 'center',
         marginBottom: 10
     },
-    textInfo:{
+    textInfo: {
         fontSize: 18,
         color: 'white',
     },
--- projetMeteo/Pages/Home/index.tsx
+++ projetMeteo/Pages/Home/index.tsx
@@ -1,97 +1,145 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Image, ImageBackground, Alert, TouchableOpacity } from 'react-native';
-import Geolocation from '@react-native-community/geolocation';
+import { StyleSheet, Text, View, TextInput, Button, Image, ImageBackground, Alert, TouchableOpacity, FlatList } from 'react-native';
+import * as Location from 'expo-location';
+
+const Item = ({ ville }: any) => (
+  <View>
+    <Text>{ville}</Text>
+  </View>
+);
 
 const Home = ({ navigation }: any) => {
   var [ville, setVille] = useState('');
   var [erreur, setErreur] = useState('');
+  const [filteredVille, setFilteredVille] = useState([]);
+  const [villeAuto, setVilleAuto] = useState([]);
 
   const apiKey = "032cf4f8f4ea0479f5a02354c7508195";
   var MeteoTableau: { Timestamp: any, Date: any; Temps: any; Temperature: any; Image: any }[] = [];
+  var tableauVilleAuto: { Ville: any }[] = [];
+
+  tableauVilleAuto.push({ Ville: "test1" })
+  tableauVilleAuto.push({ Ville: "test2" })
+  tableauVilleAuto.push({ Ville: "test3" })
 
   const Recherche = () => {
     MeteoTableau = [];
 
     // console.log(ville);
-    if(ville == ""){
+    if (ville == "") {
       setErreur("Veuillez rentrer un nom de ville");
-    }else{
+    } else {
       fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + ville + '&units=metric&appid=' + apiKey)
-      .then((response) => response.json())
-      .then((json) => {
-        if(json.message == "city not found"){
-          setErreur("La ville n'a pas été trouvé");
-        }else{
-          setErreur("");
-          var date = null;
-          var temps = null;
-          var temperature = null;
-    
-          for(let k of json.list){
-            //console.log(date);
-            if(date == null){
-              date = k.dt_txt;
-              var dateSplit = date.split(' ');
-              date = dateSplit[0];
-              temps = k.weather[0].main;
-              temperature = k.main.temp;
-            
-              var dateSplitFormat = date.split('-');
-              var dateFinal = dateSplitFormat[2]+"/"+dateSplitFormat[1]+"/"+dateSplitFormat[0];
-              
-              var timeStamp = k.dt;
+        .then((response) => response.json())
+        .then((json) => {
+          if (json.message == "city not found") {
+            setErreur("La ville n'a pas été trouvé");
+          } else {
+            setErreur("");
+            var date = null;
+            var temps = null;
+            var temperature = null;
 
-              MeteoTableau.push({Timestamp: timeStamp ,Date: dateFinal, Temps: temps, Temperature: temperature, Image: null});
-            }else{
-              var date2 = k.dt_txt;
-              var dateSplit2 = date2.split(' ');
-              date2 = dateSplit2[0];
-              if(date == date2){
-                //Même jour
-              }else{
-                date = date2;
+            for (let k of json.list) {
+              //console.log(date);
+              if (date == null) {
+                date = k.dt_txt;
+                var dateSplit = date.split(' ');
+                date = dateSplit[0];
                 temps = k.weather[0].main;
                 temperature = k.main.temp;
-                
-                var date2SplitFormat = date2.split('-');
-                var dateFinal = date2SplitFormat[2]+"/"+date2SplitFormat[1]+"/"+date2SplitFormat[0];
-    
+
+                var dateSplitFormat = date.split('-');
+                var dateFinal = dateSplitFormat[2] + "/" + dateSplitFormat[1] + "/" + dateSplitFormat[0];
+
                 var timeStamp = k.dt;
 
-                MeteoTableau.push({Timestamp: timeStamp, Date: dateFinal, Temps: temps, Temperature: temperature, Image: null});
+                MeteoTableau.push({ Timestamp: timeStamp, Date: dateFinal, Temps: temps, Temperature: temperature, Image: null });
+              } else {
+                var date2 = k.dt_txt;
+                var dateSplit2 = date2.split(' ');
+                date2 = dateSplit2[0];
+                if (date == date2) {
+                  //Même jour
+                } else {
+                  date = date2;
+                  temps = k.weather[0].main;
+                  temperature = k.main.temp;
+
+                  var date2SplitFormat = date2.split('-');
+                  var dateFinal = date2SplitFormat[2] + "/" + date2SplitFormat[1] + "/" + date2SplitFormat[0];
+
+                  var timeStamp = k.dt;
+
+                  MeteoTableau.push({ Timestamp: timeStamp, Date: dateFinal, Temps: temps, Temperature: temperature, Image: null });
+                }
               }
+
             }
-    
+            navigation.navigate("info", { ville: ville, tableau: MeteoTableau });
           }
-          navigation.navigate("info", { ville: ville, tableau: MeteoTableau });
-        }
-      })
+        })
     }
   }
 
   const Geolocalisation = async () => {
-    var lat = 0;
-    var lon = 0;
+    // var lat = 0;
+    // var lon = 0;
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      let location = await Location.getCurrentPositionAsync({});
+      var lat = location.coords.latitude;
+      var lon = location.coords.longitude;
 
-    Geolocation.getCurrentPosition(
-      info => {
-        lat = info.coords.latitude
-        lon = info.coords.longitude
-
-        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat.toString() + '&lon=' + lon.toString() + '&appid=' + apiKey)
+      fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat.toString() + '&lon=' + lon.toString() + '&appid=' + apiKey)
         .then((response) => response.json())
         .then((json) => {
           //console.log(json.name);
           ville = json.name;
           Recherche();
         })
-      } 
-    );
+    }
   }
 
-  const favoris = () => {  
+  const favoris = () => {
     navigation.navigate("favoris");
+  }
+
+  const apiGouvAdress = (search: any) => {
+    tableauVilleAuto = [];
+
+    fetch('https://api-adresse.data.gouv.fr/search/?q=' + search)
+      .then((response) => response.json())
+      .then((json) => {
+        //console.log(json.features);
+        // tableauVilleAuto = [];
+
+        for (let k of json.features) {
+          //console.log(k);
+          tableauVilleAuto.push({ Ville: k.properties.city })
+        }
+        //console.log(tableauVilleAuto);
+      })
+  }
+
+  const trouverVille = (query: any) => {
+    setVille(query)
+    // if (query) {
+    //   const regex = new RegExp(`${query.trim()}`, 'i');
+    //   apiGouvAdress(regex);
+    // }
+  };
+
+  const renderItem = ({ item }: any) => (
+    <TouchableOpacity onPress={AutoCompleteSelection}>
+      <Item ville={item.Ville} />
+    </TouchableOpacity>
+
+  );
+
+  const AutoCompleteSelection = () => {
+    console.log("test");
   }
 
   return (
@@ -108,9 +156,15 @@ const Home = ({ navigation }: any) => {
         <TextInput
           style={styles.input}
           placeholder="Nom de la ville"
-          onChangeText={newVille => setVille(newVille)}
+          onChangeText={newVille => trouverVille(newVille)}
           keyboardType="default"
         />
+
+        {/* <FlatList
+          data={tableauVilleAuto}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.Ville}
+        /> */}
 
         <Text>{erreur}</Text>
 
@@ -121,7 +175,7 @@ const Home = ({ navigation }: any) => {
             onPress={Recherche}
           />
         </TouchableOpacity>
-        
+
         <Button
           color="green"
           title="Me géolocaliser"
@@ -129,10 +183,10 @@ const Home = ({ navigation }: any) => {
         />
 
         <TouchableOpacity onPress={favoris} style={styles.favorisDiv}>
-          <Text>Voir ses favoris 
+          <Text>Voir ses favoris
             <Image
-            style={styles.favoris}
-            source={require('../../assets/favoris.png')}
+              style={styles.favoris}
+              source={require('../../assets/favoris.png')}
             />
           </Text>
         </TouchableOpacity>
@@ -144,41 +198,41 @@ const Home = ({ navigation }: any) => {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      // justifyContent: 'center',
-    },
-    input: {
-      height: 40,
-      width: 300,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    },
-    image: {
-      width: 100,
-      height: 100
-    },
-    textTitre:{
-      fontSize: 20
-    },
-    imageBackground:{
-      flex: 1,
-      justifyContent: "center"
-    },
-    buttonValider:{
-      marginBottom: 10
-    },
-    favoris:{
-      width: 20,
-      height: 20,
-    },
-    favorisDiv:{
-      justifyContent: 'flex-end',
-      alignItems: 'flex-end'
-    }
-  });
-  
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  image: {
+    width: 100,
+    height: 100
+  },
+  textTitre: {
+    fontSize: 20
+  },
+  imageBackground: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  buttonValider: {
+    marginBottom: 10
+  },
+  favoris: {
+    width: 20,
+    height: 20,
+  },
+  favorisDiv: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+  }
+});
+
 
 export default Home;
